6b92aa56f12e17aab53f0bc2a6d391fffba113f5
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2015-05-31 16:33:12 +0200

    dfas

---
+++ SubNetwork/SubNetwork/AdministrativeDomain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using networkLibrary;
+
+namespace SubNetwork
+{
+    class AdministrativeDomain
+    {
+        private transportClient signalization;
+        private networkLibrary.transportServer.NewMsgHandler msgHandler;
+        private Dictionary<string, string> directory;
+
+        public AdministrativeDomain()
+        {
+            signalization = new transportClient("localhost", "3333");
+            msgHandler = new transportServer.NewMsgHandler(newMessageReceived);
+            directory = new Dictionary<string, string>();
+            signalization.sendMessage("NCC1#");
+        }
+
+        #region message received methods
+        private void newMessageReceived(object a, MessageArgs e) { }
+        private void nameCastReceived(string message) { }
+        private void callCoordinationReceived(string message) { }
+        private void callRequestReceived(string message) { }
+        #endregion
+    }
+}
---
+++ SubNetwork/SubNetwork/LRM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubNetwork
+{
+    class LRM
+    {
+        private Dictionary<String, OneLRM> resources;
+        private String domain;
+
+        public LRM(String domain)
+        {
+            this.domain = domain;
+            resources = new Dictionary<String, OneLRM>();
+        }
+
+        public void addLRM(String subConnection, List<String> portIn, List<String> portOut)
+        {
+            resources.Add(subConnection, new OneLRM(portIn, portOut));
+        }
+
+        public String Domain
+        {
+            get { return domain; }
+        }
+    }
+
+    class OneLRM{
+        public List<String> portIn { get; set; }
+        public List<String> portOut { get; set; }
+        public OneLRM(List<String> portIn, List<String> portOut)
+        {
+            this.portIn = portIn;
+            this.portOut = portOut;
+        }
+    }
+}
